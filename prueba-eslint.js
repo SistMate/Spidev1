2ddfca7a995bf6a693c8a7fa4bf9b8db4897d975
Mateo Villagomez <mateovillagomez2@gmail.com>
2025-11-03 13:48:51 -0400

    Delete prueba-eslint.js

--- prueba-eslint.js
+++
@@ -1,8 +0,0 @@
-// Archivo de prueba para ESLint
-function suma(a, b) {
-    let resultado = a + b
-    let sinUsar = 123; // Variable sin usar (ESLint lo detecta)
-    return resultado;
-}
-
-console.log('La suma es:', suma(2, 3));
