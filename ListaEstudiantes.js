b57c6d1c762bd6b7df7972968255d99590cdf3ea
Mateo Villagomez <mateovillagomez2@gmail.com>
2025-12-09 18:08:05 -0400

    Update ListaEstudiantes.js

--- ListaEstudiantes.js
+++ ListaEstudiantes.js
@@ -1,26 +1,34 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  try {
-    const res = await fetch("http://localhost:3000/listar-estudiantes");
-    const data = await res.json();
-
-    if (data.success) {
-      const tbody = document.querySelector("#tablaEstudiantes tbody");
-
-      data.usuarios.forEach(est => {
-        const fila = document.createElement("tr");
-        fila.innerHTML = `
-          <td>${est.id}</td>
-          <td>${est.nombre_completo}</td>
-          <td>${est.email}</td>
-          <td>${est.rol}</td>
-        `;
-        tbody.appendChild(fila);
-      });
-    } else {
-      alert("No se pudieron cargar los estudiantes.");
-    }
-  } catch (err) {
-    console.error("Error al obtener estudiantes:", err);
-    alert("Error de conexión con el servidor.");
-  }
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("JS CARGADO");
+  cargarEstudiantes();
 });
+
+async function cargarEstudiantes() {
+  try {
+    const res = await fetch("http://localhost:3000/estudiantes");
+
+    if(!res.ok){
+      throw new Error("Error HTTP: " + res.status);
+    }
+
+    const estudiantes = await res.json();
+    console.log("Datos recibidos:", estudiantes);
+
+    const tbody = document.querySelector("#tablaEstudiantes tbody");
+    tbody.innerHTML = "";
+
+    estudiantes.forEach(est => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${est.nombre_completo|| ""}</td>
+        <td>${est.email || ""}</td>
+        <td>${est.rol || ""}</td>
+      `;
+      tbody.appendChild(tr);
+    });
+
+  } catch (error) {
+    console.error("Error al cargar estudiantes:", error);
+    alert("Error cargando datos, revisa la consola (F12)");
+  }
+}
