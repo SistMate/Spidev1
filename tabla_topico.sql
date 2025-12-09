7378319722eb87f41b19cf72f202b4d5d83df732
Mateo Villagomez <mateovillagomez2@gmail.com>
2025-12-09 18:16:20 -0400

    Delete tabla_topico.sql
    Ya no es necesaria

--- tabla_topico.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE topico (
-    id SERIAL PRIMARY KEY,              
-    nombre VARCHAR(255) NOT NULL,      
-    descripcion TEXT,                  
-    url_video TEXT,                     
-    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP  
-);
